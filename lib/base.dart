548e13c5b22ce4aee0b0658ca5e9510ceadaf571
Dz <MathBeWithCode@gmail.com>
2025-12-02 20:13:54 +0800

    init

---
+++ lib/base.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: camel_case_types
+
+typedef float = double;
+
+class Ref<T> {
+  final List<T> _ref;
+  Ref(T value) : _ref = [value];
+  T get value => _ref[0];
+  set value(T newValue) => _ref[0] = newValue;
+}
